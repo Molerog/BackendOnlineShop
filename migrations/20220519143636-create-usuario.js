e63d6b3f3836b40337e31fbebf23331940782167
Molerog <102958319+Molerog@users.noreply.github.com>
2022-05-19 16:58:32 +0200

    base de datos y tabla usuarios creada

---
+++ migrations/20220519143636-create-usuario.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Usuarios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombre: {
+        type: Sequelize.STRING
+      },
+      apellido: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      dni: {
+        type: Sequelize.STRING
+      },
+      fechaDeAlta: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      fechaDeActualizacion: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Usuarios');
+  }
+};
