0c0a44c4567ab6f0e7188246017c7b1ca0b98641
Molerog <102958319+Molerog@users.noreply.github.com>
2022-05-19 17:21:12 +0200

    puerto creado

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Logger = require('logplease');
 const options = {
     useColors: true,     // Enable colors
-    color:Logger.Colors.Blue, // Set the color of the logger
+    color:Logger.Colors.Yellow, // Set the color of the logger
     showTimestamp: true, // Display timestamp in the log message
     useLocalTime: false, // Display timestamp in local timezone
     showLevel: true,     // Display log level in the log message
@@ -9,6 +9,13 @@ const options = {
     appendFile: true,    // Append logfile instead of overwriting
   };
 const logger = Logger.create('Mensaje',options);
-logger.info("probando")
+const express = require("express");
+const app = express();
+const port = 3000;
 
-console.log("holaaaa")
+app.use(express.json())
+
+app.listen(port,()=>{
+    logger.info("Servidor corriendo en el puerto " + port)
+})
+
