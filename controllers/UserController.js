f664c3c1c1ea2a02c01779530e989ecc4e4ceb13
Vincorp21 <vibarcar@gmail.com>
2022-05-20 13:11:25 +0200

    endpoint user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,28 +1,42 @@
-const {User} = require('../models/index');
+const { User } = require('../models/index');
 
 const UserController = {
-    async create(req,res) {
+  async create(req, res) {
     try {
-        const user = await User.create({...req.body})
-        req.body.role = "user"
-        res.status(201).send({mensaje: "User added...", user})
+      const user = await User.create({ ...req.body });
+      req.body.role = 'user';
+      res.status(201).send({ mensaje: 'User added...', user });
     } catch (error) {
-        console.log(error);
-        res.status(500).send({
-            mensaje:
-            "We had an issue creating the user..."
-        })
+      console.log(error);
+      res.status(500).send({
+        mensaje: 'We had an issue creating the user...',
+      });
     }
-    },
-    async getAll(req,res){
-        try {
-          const users = await User.findAll()
-           res.status(201).send({mensaje: 'Search completed...', users})
-        } catch (error) {
-            console.log(error)
-            res.status(500).send({mensaje: "We had an issue searching the table..."})
-        }
+  },
+  async getAll(req, res) {
+    try {
+      const users = await User.findAll();
+      res.status(201).send({ mensaje: 'Search completed...', users });
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .send({ mensaje: 'We had an issue searching the table...' });
     }
-}
+  },
+  async getUserOrder(req, res) {
+    try {
+      const users = await User.findAll({
+        include: [Order],
+      });
+      res.status(201).send({ mensaje: 'Show Users with Orders' });
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .send({ mensaje: ' We had a problem lookinf for Users with orders ' });
+    }
+  },
+};
 
-module.exports = UserController
+module.exports = UserController;
