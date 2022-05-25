00253534c817caa5315a9fbf5200289a1d1d54a7
Vincorp21 <vibarcar@gmail.com>
2022-05-25 16:58:33 +0200

    prueba seed

--- seeders/20220522220140-demo-order_product.js
+++ seeders/20220522220140-demo-order_product.js
@@ -1,41 +1,41 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert ( 'Order_Products', [
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Order_Products', [
       {
         OrderId: 1,
-        ProductId: 3,
+        ProductId: [1, 2, 3],
         createdAt: new Date(),
-        updatedAt: new Date()   
+        updatedAt: new Date(),
       },
       {
         OrderId: 2,
-        ProductId:1,
+        ProductId: 1,
         createdAt: new Date(),
-        updatedAt: new Date()   
+        updatedAt: new Date(),
       },
       {
         OrderId: 3,
-        ProductId:4,
+        ProductId: 4,
         createdAt: new Date(),
-        updatedAt: new Date()   
+        updatedAt: new Date(),
       },
       {
         OrderId: 3,
-        ProductId:1,
+        ProductId: 1,
         createdAt: new Date(),
-        updatedAt: new Date()   
+        updatedAt: new Date(),
       },
-    ])
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
