0d4427d1e28f72f81384d324924a4b6e5c7a266d
Molerog <102958319+Molerog@users.noreply.github.com>
2022-05-26 13:38:15 +0200

    correciones menores

--- seeders/20220520112234-demo-order.js
+++ seeders/20220520112234-demo-order.js
@@ -4,18 +4,18 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     return queryInterface.bulkInsert ( 'Orders', [
       {
-        order_num: 1 ,
-        date: "1987-07-25" ,
-        UserId: 1,        
-        createdAt: new Date(),
-        updatedAt: new Date()
+      order_num: 1 ,
+      date: "1987-07-25" ,
+      UserId: 1,        
+      createdAt: new Date(),
+      updatedAt: new Date()
       },
       {
       order_num: 2 ,
-        date: "1999-07-25",
-        UserId: 2  ,      
-        createdAt: new Date(),
-        updatedAt: new Date()
+      date: "1999-07-25",
+      UserId: 2  ,      
+      createdAt: new Date(),
+      updatedAt: new Date()
       },
       {
       order_num:3,
@@ -25,39 +25,39 @@ module.exports = {
       updatedAt: new Date()
       },
       {
-        order_num:4,
-        date: "2030-07-25",
-        UserId: 1,
-        createdAt: new Date(),
-        updatedAt: new Date()
+      order_num:4,
+      date: "2030-07-25",
+      UserId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
         },
         {
-          order_num:5,
-          date: "2030-07-25",
-          UserId: 4,
-          createdAt: new Date(),
-          updatedAt: new Date()
+      order_num:5,
+      date: "2030-07-25",
+      UserId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
           },
           {
-            order_num:6,
-            date: "2030-07-25",
-            UserId: 5,
-            createdAt: new Date(),
-            updatedAt: new Date()
+      order_num:6,
+      date: "2030-07-25",
+      UserId: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
             },
           {
-            order_num:7,
-            date: "2030-07-25",
-            UserId: 5,
-            createdAt: new Date(),
-            updatedAt: new Date()
+      order_num:7,
+      date: "2030-07-25",
+      UserId: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
             },
          {
-            order_num:8,
-            date: "2030-07-25",
-            UserId: 3,
-            createdAt: new Date(),
-            updatedAt: new Date()
+      order_num:8,
+      date: "2030-07-25",
+      UserId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
             },    
     ])   
   },
